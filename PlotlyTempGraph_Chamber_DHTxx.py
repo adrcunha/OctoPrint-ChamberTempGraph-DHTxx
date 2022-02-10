051f5aeb4b51229b1bb716956e85b0bdb9c327a8
adrcunha <adrcunha@gmail.com>
2022-02-09 22:39:42 -0800

    Rename dht_enclosure_temp.py to PlotlyTempGraph_Chamber_DHTxx.py.

--- PlotlyTempGraph_Chamber_DHTxx.py
+++ PlotlyTempGraph_Chamber_DHTxx.py
@@ -5,9 +5,11 @@ import octoprint.util
 import Adafruit_DHT
 
 #
-# Plot enclosure temperature with OctoPrint-PlotlyTempGraph plugin.
+# Plot chamber temperature with OctoPrint-PlotlyTempGraph plugin.
 #
-# Enclosure temperature is measured by a DHT11/DHT22 sensor conhected to the GPIO
+# NOTE: As of 2/2022, PlotlyTempGraph is incompatible with TouchUI plugin.
+#
+# Chamber temperature is measured by a DHT11/DHT22 sensor conhected to the GPIO
 # pin defined by the PIN constant below.
 #
 # Based on https://github.com/jneilliii/OctoPrint-PlotlyTempGraph/blob/master/klipper_additional_temp.py
@@ -24,7 +26,7 @@ GPIO = 23
 SENSOR = Adafruit_DHT.DHT22
 
 
-class DHTEnclosureTemp(octoprint.plugin.StartupPlugin, octoprint.plugin.RestartNeedingPlugin):
+class DHTChamberTemp(octoprint.plugin.StartupPlugin, octoprint.plugin.RestartNeedingPlugin):
 	def __init__(self):
 		self.last_dht_temp = dict()
 		# Be on the safe side (because of clones) and only sample DHTXX every 3 seconds.
@@ -34,16 +36,16 @@ class DHTEnclosureTemp(octoprint.plugin.StartupPlugin, octoprint.plugin.RestartN
 		# Read quickly as possible, skip if error (temperature won't update in the graph)
 		humidity, temperature = Adafruit_DHT.read(SENSOR, GPIO)
 		if humidity is not None and temperature is not None:
-			self.last_dht_temp["Enclosure"] = (temperature, None)
+			self.last_dht_temp["Chamber"] = (temperature, None)
 
 	def dht_temp_callback(self, comm, parsed_temps):
 		parsed_temps.update(self.last_dht_temp)
 		return parsed_temps
 
-__plugin_name__ = "Enclosure Temperature"
+__plugin_name__ = "Chamber Temperature"
 __plugin_pythoncompat__ = ">=2.7,<4"
 __plugin_version__ = "0.0.1"
-__plugin_implementation__ = DHTEnclosureTemp()
+__plugin_implementation__ = DHTChamberTemp()
 __plugin_hooks__ = {
 	"octoprint.comm.protocol.temperatures.received": __plugin_implementation__.dht_temp_callback
 }
--- README.md
+++ README.md
@@ -1,8 +1,12 @@
-# OctoPrint-PlotlyTempGraph-DHT11
+# OctoPrint-ChamberTempGraph-DHTxx
 
-This plugin plots the enclosure temperature through OctoPrint-PlotlyTempGraph plugin.
+## PlotlyTempGraph_Chamber_DHTxx.py
 
-Enclosure temperature is measured by a DHT11/DHT22 sensor conhected to a GPIO
+This plugin plots the chamber temperature through OctoPrint-PlotlyTempGraph plugin.
+
+**NOTE:** As of 2/2022, PlotlyTempGraph is incompatible with TouchUI plugin.
+
+Chamber temperature is measured by a DHT11/DHT22 sensor conhected to a GPIO
 pin defined by the `PIN` constant in the code.
 
 Based on https://github.com/jneilliii/OctoPrint-PlotlyTempGraph/blob/master/klipper_additional_temp.py
